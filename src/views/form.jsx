68674516dafac9d9b7f7329dc45ca4cfe1cc59ad
Jorge <jorge.22fv@gmail.com>
2022-06-20 08:35:58 -0400

    add validation

--- src/views/form.jsx
+++ src/views/form.jsx
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom"
 import { Context } from '../store/appContext';
 
 export const Form = () => {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [age, setAge] = useState("")
-
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState("");
+    const [error, setError] = useState("");
     const { actions } = useContext(Context);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        if (name === "" || age === "" || email === "") {
+            e.preventDefault()
+            setError("Debes completar todos los campos")
+        }
         const userInfo = { name, email, age }
         actions.setData(userInfo)
     }
-
     return (
         <div>
+            {error !== "" && <div className="alert alert-danger m-5" role="alert">
+                {error}
+            </div>}
             <form className="form border rounded col-md-5 mt-5">
                 <div className="m-3 text-start">
                     <label className="form-label" >Nombre</label>
@@ -33,7 +39,7 @@ export const Form = () => {
                         onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <Link to="/resultForm" type="button" className="btn btn-outline-dark mb-5 m-auto"
-                    onClick={handleSubmit}>Ingresar</Link>
+                    onClick={(e) => handleSubmit(e)}>Ingresar</Link>
             </form>
             <Link to="/userList" type="button" className="btn btn-outline-dark m-5">Ver contactos</Link>
         </div>
